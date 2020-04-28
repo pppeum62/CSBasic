9d32c733865579669a5cfddaa40dbb323730db47
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-04-28 14:21:50 +0900

    C# 기본 연습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,28 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            var name = "윤" + "인" + "성";
+            Console.WriteLine("Hello C#");
+            int add = 2;
+            int Alpha;
+
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello ");
+            Console.Write("World!");
+            Console.WriteLine("");
+
+            Console.WriteLine(52);
+            Console.WriteLine(7 % 3);
+            Console.WriteLine('A');
+            Console.WriteLine("A");
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"");
+            Console.WriteLine("가" + "나" + "다");
+
+            Console.WriteLine(true);
+            Console.WriteLine(false);
         }
     }
 }
